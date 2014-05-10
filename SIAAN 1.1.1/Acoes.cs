b2141b2dde28a136eafcb260c063931fd0ff2333
TrYPPF <salomaodavi74@yahoo.com.br>
2014-05-10 11:16:19 -0300

    Update Acoes.cs

--- SIAAN 1.1.1/Acoes.cs
+++ SIAAN 1.1.1/Acoes.cs
@@ -1,131 +1,183 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using SIAAN_1._1;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Speech.Recognition;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Management;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
 
 namespace SIAAN_1._1._1
 {
-    class Acoes
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      public string texto {get; set;}
-
-        public int desligarmento=0;
-        public int iraoppf = 0;
-        public int fechar_visual=0;
-
-        public MainWindow janela {get;set;}
-        public TextBox retornno {get;set;}
-
-        public Slider slider { get; set; }
-        public void reconhecer()
+        Reconhecer reconhecedor;
+        string conteudo;
+        StringBuilder cons = new StringBuilder();
+        
+        public MainWindow()
         {
+            InitializeComponent();
+            
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
 
-              if ((texto=="positivo"||texto == "sim") && desligarmento == 1)
-             {
-                 falar("Desligando...");
-                 Process.Start("shutdown", "-f -t 0 -s");
-                 desligarmento = 0;
-             }
-              if (desligarmento == 1 && (texto == "negativo" || texto == "não"))
-             {
-                 falar("Processo de desligamento cancelado!");
-                 desligarmento = 0;
-             }
-             if ((texto == "positivo" || texto == "sim") && fechar_visual == 1)
-             {
-                 falar("Fechando...");
-                 Process[] pro = Process.GetProcessesByName("devenv");
+            key.CreateSubKey("SIAAN");
+            key = key.OpenSubKey("SIAAN", true);
 
-                 janela.Close();
-                 foreach (Process pp in pro)
-                 {
-                     pp.Kill();
-                 }
-                 fechar_visual = 0;
-             }
-             if (fechar_visual == 1 && (texto == "negativo" || texto == "não"))
-             {
-                 falar("Processo de fechamento cancelado!");
-                fechar_visual = 0;
-             }
-             if ((texto == "positivo" || texto == "sim") && iraoppf == 1)
-             {
-                 falar("Abrindo página web!");
-                 Process.Start("http://www.power-pixel.net/t57094-siaan-comandos-versao-teste");
-                 iraoppf = 0;
-             }
-             if (iraoppf == 1 && (texto == "negativo" || texto == "não"))
-             {
-                 falar("Cancelado!!");
-                 iraoppf = 0;
-             }
-             try
-             {
-                 switch (texto)
-                 {
-                     case "fale sobre o sistema operacional":
-                         StringBuilder eae = new StringBuilder();
-                         eae.AppendLine(String.Format("Nome: {0}",Environment.OSVersion));
-                         eae.AppendLine(String.Format("Arquitetura: {0} bits", Environment.Is64BitOperatingSystem?"64":"32"));
-                         retornno.Text += eae;
-                         falar("Informações adicionadas a área de texto.");
-                         break;
-                     case "cite os planetas do sistema solar":
-                       
-                         falar("Sim senhor, os planetas rochosos são, a partir da distância do Sol: Mercúrio, Vênus, Terra, Marte. E os planetas jupiterianos, os gigantes gasosos: Júpiter, Saturno, Urano e Netuno. Plutão foi rebaixado a planeta-anão em 2006.");
-                         break;
-                     case "gin defina a equivalência massa-energia":
-                         falar("Foi uma teoria de Albert Einstein que massa e energia estão sempre associados, a equação é:  É igual a mc ao quadrado, isto é, a energia em Jaules é igual a massa vezes a luz no vácuo ao quadrado, velocidade da luz no vácuo é 299792458 metros por segundo!");
-                         break;
-                     case "falas presentes":
-                         falar("O senhor deseja que eu liste as falas presentes? Necessito abrir uma págna web!");
-                         iraoppf = 1;
-                         break;
-                     case "ajuda aqui":
-                         falar("Caro senhor, meu nome é Gin, este é o projeto SIAAN, ele é um projeto com finalidade de criar uma assitente virtual que reconheça voz e face para facilitar diversos trabalhos, como esta é a versão de teste o Design e funcionalidades são limitadas. Breve o será adicionado mais comandos, outros idiomas e um melhor design. Existem alguns comandos secretos nesse programa, tente descobri-los!");
-                         break;
-                     case "desligar a máquina":
-                         falar("O senhor deseja mesmo desligar a máquina?");
-                         desligarmento = 1;
-                         break;
-                     case "gin que horas são?":
-                         falar("Senhor, são: " + DateTime.Now.Hour + " e " + DateTime.Now.Minute + "");
 
-                         break;
-                     case "gin feche o programa":
-                         falar("Até mais senhor!");
-                         janela.Close();
-                         break;
-                     case "fala teste":
-                         falar("Escutei sua voz , tudo funcionando 100%");
-                         break;
-                     case "gin informe o clima":
-                         Funcionalidades ee = new Funcionalidades();
-                         ee.obter_Temperatura("http://weather.yahooapis.com/forecastrss?w=455825&u=c", true, true, true);
-                         falar(ee.retorno);
-                         break;
-                     case "feche o visual studio":
-                         falar("O senhor deseja mesmo fechar o visual studio forçado?");
-                         fechar_visual = 1;
-
-                         break;
-                 }
-             }
-             catch (InvalidOperationException eee)
-             {
-                 MessageBox.Show("Um erro ocorreu: \n\n" + eee.Message, "Erro...", MessageBoxButton.OK, MessageBoxImage.Error);
-
-             }
-             catch (Exception eee)
-             {
-                 MessageBox.Show("Um erro ocorreu: \n\n" + eee.Message, "Erro...", MessageBoxButton.OK, MessageBoxImage.Error);
-             }
+            if (key.GetValue("Informado") == null)
+            {
+                key.SetValue("Informado", "ok");
+                if (MessageBox.Show("Baixe os pacotes necessários para o programa funcionar.", "Alerta", MessageBoxButton.OK, MessageBoxImage.Information) == MessageBoxResult.OK)
+                {
+                    pacotesdown.Focus();
                 }
+            }
+
+            
+
+            Application.Current.SessionEnding += Current_SessionEnding;
+        }
+
+        void Current_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+        {
+            if (e.ReasonSessionEnding == ReasonSessionEnding.Shutdown)
+            {
+                if (MessageBoxResult.No == MessageBox.Show("Deseja mesmo fechar o SIAAN?","Fechar o SIAAN",MessageBoxButton.YesNo,MessageBoxImage.Question))
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    e.Cancel = false;
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+       
+
+      
+        private void falar_botao_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                reconhecedor = new Reconhecer();
+
+                parar_botao.IsEnabled = true;
+
+                falar_botao.IsEnabled = false;
+                SpeechRecognitionEngine engine = reconhecedor.engine;
+
+                engine.RecognizeCompleted += engine_RecognizeCompleted;
+
+                engine.SpeechRecognized += engine_SpeechRecognized;
+
+                try
+                {
+                    string linha ;
+                    StreamReader falas = new StreamReader(Environment.CurrentDirectory + @"\Falas.txt");
+                    List<string> ressu = new List<string>();
+                    while((linha=falas.ReadLine())!=null){
+                       //if(Regex.IsMatch(linha, @"^[a-zA-Z\s]+$")){
+ressu.Add(linha);
+                       //}
+                       
+                        
+                    }
+                    /*if (!ressu.Any())
+                    {
+                        MessageBox.Show("Por favor, só é permitido no arquivo de falas letras, números e espaços", "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);  
+                    }*/
+                    falas.Close();
+                    if (ressu.Any())
+                    {
+                        reconhecedor.escolhas = ressu.ToArray();
+                        reconhecedor.gramatica = new GrammarBuilder();
+
+
+
+
+
+                        reconhecedor.Recognize(RecognizeMode.Multiple);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não há nada neste arquivo.", "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        
+                    }
+                    
+                    //Regex.IsMatch(input, @"^[a-zA-Z]+$");
+                }
+                catch (InvalidOperationException ee)
+                {
+                    
+                    MessageBox.Show("Um erro ocorreu: \n\n" + ee.Message, "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (FileNotFoundException ee)
+                {
+                    MessageBox.Show("Um erro ocorreu: \n\n" + ee.Message, "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                //reconhecedor.escolhas = new string[] {"fale sobre o sistema operacional","cite os planetas do sistema solar","feche o visual studio","gin defina a equivalência massa-energia","ajuda aqui", "desligar a máquina","gin feche o programa","fala teste","ajuda aqui","gin informe o clima" ,"positivo","sim","não","negativo","falas presentes"};
+
+               
+            }
+            catch (InvalidOperationException ee)
+            {
+                switch (ee.Message)
+                {
+                    
+                    case "Cannot find the requested data item, such as a data key or value.":
+                        MessageBox.Show("Um erro ocorreu, talvez você não tenha um dispositivo de gravação conectado. \n\n"+ee.Message,"Erro!",MessageBoxButton.OK,MessageBoxImage.Error);
+                        parar_botao.IsEnabled = false;
+                falar_botao.IsEnabled = true;
+                        break;
+                    default:
+                        MessageBox.Show("Um erro ocorreu: \n\n" + ee.Message, "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+
+                }
+                
+                
+            }
+            catch (Exception eee)
+            {
+                MessageBox.Show("Um erro ocorreu: \n\n" + eee.Message, "Erro...", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        
+
+
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+           Acoes acoes = new Acoes();
+            acoes.janela = janela;
+            acoes.retornno = retornno;
+            acoes.slider = slider;
+            acoes.texto = e.Result.Text;
+            acoes.reconhecer();
+        }
 
         private void falar(string p)
         {
@@ -142,19 +194,175 @@ namespace SIAAN_1._1._1
             }
             catch (InvalidOperationException e)
             {
-
+                
                 MessageBox.Show("Um erro ocorreu: \n\n" + e.Message, "Erro...", MessageBoxButton.OK, MessageBoxImage.Error);
-
+            
             }
             catch (Exception eee)
             {
                 MessageBox.Show("Um erro ocorreu: \n\n" + eee.Message, "Erro...", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        void engine_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            parar_botao.IsEnabled = false;
+            falar_botao.IsEnabled = true;
         }
+        private void parar_botao_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                reconhecedor.engine.RecognizeAsyncCancel();
+                parar_botao.IsEnabled = false;
+                falar_botao.IsEnabled = true;
+                
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Ocorreu um erro, informe o desenvolvedor deste programa: \n" + ee.Message, "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void slider_mover(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            porcentagem_vel.Content = Math.Floor(slider.Value) + "%";
+        }
+
+        private void falarr_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(fala.Text))
+                {
+                    Fala falla = Fala.getInstance(ref falarr);
+                    falla.Velocidade = 0;
+                    falla.Volume = (int)slider.Value;
+
+                    falla.Falar(fala.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Insira um texto!", "Erro!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                
+            }
+
+            catch (Exception ee)
+            {
+                MessageBox.Show("Ocorreu um erro, informe o desenvolvedor deste programa: \n"+ee.Message,"Erro!",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void audio_criador_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Na versão de teste, este comando ainda não está disponível!","Informação",MessageBoxButton.OK,MessageBoxImage.Information);
+            
+            
+        }
+
+        private void pulae(object sender, RoutedEventArgs e)
+        {
+            string maria ;
+           string oo;
+            try
+            {
+               StreamReader cconteudo  = new StreamReader(Environment.CurrentDirectory + @"\falas.txt");
+
+               while((oo=cconteudo.ReadLine())!=null)
+               {
+                   cons.AppendLine(oo);
+               }
+               cconteudo.Close();
+            }
+               
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.Message);
+            }
+
+            Console.WriteLine(cons.ToString());
+            CompileAndRun(cons.ToString());
+
+        }
+        static void CompileAndRun(string code)
+        {
+            var csc = new CSharpCodeProvider();
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, "teste.exe", true)
+            {
+                GenerateExecutable = true
+            };
+            CompilerResults compiledAssembly = csc.CompileAssemblyFromSource(parameters,
+            code);
+
+            var errors = compiledAssembly.Errors
+                                         .Cast<CompilerError>()
+                                         .ToList();
+
+            if (errors.Any())
+            {
+                errors.ForEach(Console.WriteLine);
+                return;
+            }
+
+            Module module = compiledAssembly.CompiledAssembly.GetModules()[0];
+
+            Type mt = null;
+            if (module != null)
+                mt = module.GetType("Program");
+
+            MethodInfo methInfo = null;
+            if (mt != null)
+                methInfo = mt.GetMethod("Main");
+
+            if (methInfo != null)
+                Console.WriteLine(methInfo.Invoke(null, null));
+        }
+
+        private void download_reconhecimento(object sender, RoutedEventArgs e)
+        {
+
+            Process.Start("http://download.microsoft.com/download/4/0/D/40D6347A-AFA5-417D-A9BB-173D937BEED4/MSSpeech_SR_pt-BR_TELE.msi.msi");
+            
+        }
+
+        private void sdk_download(object sender, RoutedEventArgs e)
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+                Process.Start("http://download.microsoft.com/download/2/2/5/225F4CFA-8B54-41DB-98C1-47F5A300BBF6/x64_MicrosoftSpeechPlatformSDK/MicrosoftSpeechPlatformSDK.msi");
+            }
+            else
+            {
+                Process.Start("http://download.microsoft.com/download/2/2/5/225F4CFA-8B54-41DB-98C1-47F5A300BBF6/x86_MicrosoftSpeechPlatformSDK/MicrosoftSpeechPlatformSDK.msi");
+            }
+        }
+
+        private void sintese_download(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://download.microsoft.com/download/4/0/D/40D6347A-AFA5-417D-A9BB-173D937BEED4/MSSpeech_TTS_pt-BR_Heloisa.msi");
+        }
+
+        private void runtime_speech(object sender, RoutedEventArgs e)
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+                Process.Start("http://download.microsoft.com/download/A/6/4/A64012D6-D56F-4E58-85E3-531E56ABC0E6/x64_SpeechPlatformRuntime/SpeechPlatformRuntime.msi");
+            }
+            else
+            {
+                Process.Start("http://download.microsoft.com/download/A/6/4/A64012D6-D56F-4E58-85E3-531E56ABC0E6/x86_SpeechPlatformRuntime/SpeechPlatformRuntime.msi");
+            }
+        }
+        
+
+        
+        
+
+       
+        
+        
+       
     }
-
-
-
-
-
+}
